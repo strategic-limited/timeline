fb1a1604eed6a3958b1d0dd23c2110a770601308
Sergey <persalius@gmail.com>
2020-11-27 15:51:26 +0200

    Fix

--- src/timeline.js
+++ src/timeline.js
@@ -520,6 +520,8 @@ export default class Timeline extends React.Component {
                 if (k === 0 && this.props.originalStartDate.diff(0) > element.start.diff(0)) {
                   element.start = this.props.originalStartDate;
                   element.end = this.props.originalStartDate.clone().add(elDuration);
+                  element.isUpdatedStartEnd = true;
+                  element.notSelected = true;
                   items.push(element);
                 } else if (
                   mewRowsWithNewItems[el].items[k - 1] &&
@@ -527,6 +529,8 @@ export default class Timeline extends React.Component {
                 ) {
                   element.start = mewRowsWithNewItems[el].items[k - 1].end;
                   element.end = mewRowsWithNewItems[el].items[k - 1].end.clone().add(elDuration);
+                  element.isUpdatedStartEnd = true;
+                  element.notSelected = true;
                   items.push(element);
                 }
                 if (element.end.diff(0) > this.props.originalEndDate) {
